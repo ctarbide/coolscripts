4828f25c4380a77c84348ed192eba971855deb52
Cungsten Tarbide <ctarbide@tuta.io>
2021-05-31 21:15:44 -0300

    improvements to README.sh

--- bin/README.sh
+++ bin/README.sh
@@ -3,13 +3,19 @@ set -eu #x
 stamp=`date '+%Y-%m-%d_%Hh%Mm%S'`
 pwd=`pwd`
 thisdir=${pwd##*/}
+if perl -le'exit($ARGV[0] !~ m{^ 2\d{3} - \d\d - \d\d _ \d\d h \d\d m \d\d _ }ix)' -- "${thisdir}"; then
+    title=${thisdir}
+else
+    title=${stamp}_${thisdir}
+fi
 cat<<EOF
 # -*- mode:org; coding:utf-8-unix -*-
 
-#+TITLE: ${stamp}_${thisdir}
+#+TITLE: ${title}
 #+STARTUP: indent
 
 * References
+
 
 - 
 
