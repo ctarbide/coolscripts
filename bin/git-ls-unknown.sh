665ad0f5750104da6e8c190f6e9ecaf42cd72d6b
C. Tarbide <ctarbide@tuta.io>
2021-10-14 16:16:51 -0300

    do not list ".gitignore" in git-ls-unknown.sh

--- bin/git-ls-unknown.sh
+++ bin/git-ls-unknown.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # git status --porcelain | perl -lne'next unless m{^\?\? }; print(q{/} . substr($_,3))'
-git ls-files -o --exclude-standard | perl -lne'print(qq{/${_}})'
+git ls-files -o --exclude-standard | perl -lne'next if m{^\.gitignore$}; print(qq{/${_}})'
