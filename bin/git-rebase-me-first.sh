fbc6c4315941b180118ac770d3ded69ebfc5d70f
C. Tarbide <ctarbide@tuta.io>
2021-11-15 19:53:24 -0300

    update nofake from ctweb and add git-rebase-me-first.sh

---
+++ bin/git-rebase-me-first.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eux
+
+git status --porcelain | awk '/^DU / {print $2}' | xargs -r git rm -f
+git restore -S .
+git checkout .
+
+# is working tree in pristine state?
+is_ok_to_continue_1(){
+    git diff --quiet
+}
+
+# are there unknown files?
+is_ok_to_continue_2(){
+    git status --porcelain | perl -lane'exit(1) if m{^\?\? }}{'
+}
+
+if is_ok_to_continue_1 && is_ok_to_continue_2; then
+    git rebase --continue
+else
+    git status
+fi
+
+echo "all done for ${0##*/}"
