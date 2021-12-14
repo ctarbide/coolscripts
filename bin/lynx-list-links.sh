bb669bd8c7e357421953b7e71569d98d0f7162da
C. Tarbide <ctarbide@tuta.io>
2021-12-14 19:01:34 -0300

    improve lynx-list-links.sh and add run-texlive2019.sh and run-texlive2021.sh

--- bin/lynx-list-links.sh
+++ bin/lynx-list-links.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 user_agent=${USER_AGENT:-'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'}
-echo "input args: $@"
-echo
-exec lynx --dump --listonly --image_links -useragent="${user_agent}" "$@"
+for i in "$@"; do
+    echo
+    echo "input args: ${i}"
+    echo
+    set -x
+    lynx --dump --listonly --image_links -useragent="${user_agent}" "${i}"
+    set +x
+done
---
+++ bin/run-texlive2019.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+TEXLIVE_HOME=${HOME}/Ephemeral/texlive2019
+if [ -d "${TEXLIVE_HOME}/bin/x86_64-linux" ]; then
+    if ! perl -e'do { exit 0 if $_ eq qq{$ENV{TEXLIVE_HOME}/bin/x86_64-linux} } for split(q{:},$ENV{PATH}); exit 1'; then
+        PATH=${TEXLIVE_HOME}/bin/x86_64-linux:${PATH}
+        export PATH
+    fi
+    exec "$@"
+else
+    echo "ERROR: could not find texlive installation at \"${TEXLIVE_HOME}\"" 1>&2
+fi
