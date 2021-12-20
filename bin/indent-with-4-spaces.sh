a49d91f07fe326125a73445e356729e00a783ddd
C. Tarbide <ctarbide@tuta.io>
2021-12-20 18:10:04 -0300

    astyle > bcpp

--- bin/indent-with-4-spaces.sh
+++ bin/indent-with-4-spaces.sh
@@ -3,4 +3,10 @@ set -eu
 cfg=${HOME}/.bcpp.cfg
 test -f "${cfg}" || echo > "${cfg}"
 exec 2>/dev/null
-exec bcpp -ylcnc -tbcl -bcl -nbbi -nbi -s -i 4 "$@"
+# exec bcpp -ylcnc -tbcl -bcl -nbbi -nbi -s -i 4 "$@"
+exec astyle -n -s4 --style=k/r -xB -j \
+    --pad-oper --pad-comma --break-blocks --pad-header --unpad-paren \
+    --indent-after-parens \
+    --min-conditional-indent=3 \
+    --delete-empty-lines \
+    "$@"
--- bin/indent-with-tabs.sh
+++ bin/indent-with-tabs.sh
@@ -3,4 +3,10 @@ set -eu
 cfg=${HOME}/.bcpp.cfg
 test -f "${cfg}" || echo > "${cfg}"
 exec 2>/dev/null
-exec bcpp -ylcnc -tbcl -bcl -nbbi -nbi -t "$@"
+# exec bcpp -ylcnc -tbcl -bcl -nbbi -nbi -t "$@"
+exec astyle -n -T8 --style=k/r -xB -j \
+    --pad-oper --pad-comma --break-blocks --pad-header --unpad-paren \
+    --indent-after-parens \
+    --min-conditional-indent=3 \
+    --delete-empty-lines \
+    "$@"
