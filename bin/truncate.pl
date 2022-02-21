ecf5618fce8aac541877cde71e94407858de25fd
C. Tarbide <ctarbide@tuta.io>
2022-02-21 01:26:21 +0000

    improve truncate.pl and add joverc.nw example

--- bin/truncate.pl
+++ bin/truncate.pl
@@ -11,9 +11,13 @@ $SIG{__DIE__} = \&Carp::confess;
 local $\ = "\n";
 
 for my $fname (@ARGV) {
+    if (!-w $fname) {
+        print STDERR ("File \"${fname}\" is read-only, cannot truncate, skipping.");
+        next;
+    }
     my ($st_def, $st_ino, $st_mode, $st_nlink,
         $st_uid, $st_gid, $st_rdev, $st_size,
         $st_atime, $st_mtime, $st_ctime) = stat($fname);
-    truncate($fname, 0);
+    truncate($fname, 0) or Carp::carp("truncate failed for \"${fname}\".");
     utime($st_atime, $st_mtime, $fname);
 }
