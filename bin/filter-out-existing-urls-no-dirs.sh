75923e6cfbeb707e0a82ceebb3add837c5d3b714
C. Tarbide <ctarbide@tuta.io>
2023-04-09 13:07:58 +0000

    add filter-out-existing-urls-no-dirs.sh

---
+++ bin/filter-out-existing-urls-no-dirs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+thispath=`perl -MFile::Spec::Functions=rel2abs,canonpath -le'print(canonpath(rel2abs(\$ARGV[0])))' -- "${0}"`
+thisdir=${thispath%/*}
+LC_ALL=C
+export LC_ALL
+perl -lne'next if m{\?|/$}; print' -- "$@" |
+    perl -MCGI::Util=unescape -lne'next if m{%} and m{^\w+://.*/(.*?)$} and -f unescape($1); print' |
+    perl -lne'next if m{^\w+://.*/(.*?)$} and -f $1; print'
