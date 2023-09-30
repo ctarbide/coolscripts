fc21f6ce59da7f1f3ac904d12dca90afab6199d6
C. Tarbide <ctarbide@tuta.io>
2023-09-30 01:33:10 +0000

    minor improvements to seed.sh

--- bin/cstrng.sh
+++ bin/cstrng.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set -eu
-exec perl -MTime::HiRes=time,clock -MDigest::SHA=sha256,sha512 -e'
+exec perl -MConfig -MTime::HiRes=time,clock -MScalar::Util=refaddr -MDigest::SHA=sha512 -e'
+    $seed_const = sprintf(qq{[%s]\t%s:%s:%s}, $Config{cf_time}, getppid, $$, refaddr(\%ENV));
+    $seed_ccnt = 0;
     sub seed () {
         $a = clock;
         $b = $a + 1e-4;
-        $c = 0;
-        while (clock < $b) { $c++; }
-        sprintf(qq{%s\t%s\t%s\t%.9f\t%.9f\t%s}, getppid, $$, ++$n, time, $a, $c)
+        while (clock < $b) { $seed_ccnt++; }
+        sprintf(qq{%s\t%s\t%.9f\t%.9f\t%s}, $seed_const, ++$n, time, $a, $seed_ccnt)
     }
     while (1) {
-        $s = sha256(join(qq{\n}, map {seed} 1..64));
+        $s = sha512(join(qq{\n}, map {seed} 1..64));
         $k = length($s).qq{:${s},};
-        for $n (1..65536) {
+        for $n (1..1048576) {
             print(sha512(qq{${k}${n}}));
         }
     }
--- bin/seed.sh
+++ bin/seed.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -eu
-exec perl -MTime::HiRes=time,clock -le'
+exec perl -MConfig -MTime::HiRes=time,clock -MScalar::Util=refaddr -le'
+    $seed_const = sprintf(qq{[%s]\t%s:%s:%s}, $Config{cf_time}, getppid, $$, refaddr(\%ENV));
+    $seed_ccnt = 0;
     sub seed () {
         $a = clock;
         $b = $a + 1e-4;
-        $c = 0;
-        while (clock < $b) { $c++; }
-        sprintf(qq{%s\t%s\t%s\t%.9f\t%.9f\t%s}, getppid, $$, ++$n, time, $a, $c)
+        while (clock < $b) { $seed_ccnt++; }
+        sprintf(qq{%s\t%s\t%.9f\t%.9f\t%s}, $seed_const, ++$n, time, $a, $seed_ccnt)
     }
     if ($ARGV[0] =~ /^(\d+)$/) {
         $times = $1;
