8ddb729ce13c3b4efbfa2e4f40264b50fa1da824
C. Tarbide <ctarbide@tuta.io>
2023-10-01 22:44:28 +0000

    greatly improve bitops.sh speed with pack/unpack and quads

--- bin/bitops.sh
+++ bin/bitops.sh
@@ -1,11 +1,59 @@
 #!/bin/sh
-if [ x"${1}" = x0 ]; then
-    exec perl -e'open(DATA,q{<},$ARGV[1]);while(read(STDIN,$a,1)){last unless read(DATA,$b,1);print(chr(ord($a) ^ ord($b)))}' -- "$@"
-fi
-if [ $# -eq 1 ]; then
-    exec perl -e'$c=$ARGV[0]^0xff;while(read(STDIN,$_,1)){$a=ord;print(chr(($a & $c) ^ $a))}' -- "$@"
-fi
-exec perl -e'
-    $c=$ARGV[0]^0xff;
-    open(DATA,q{<},$ARGV[1]);
-    while(read(STDIN,$a,1)){last unless read(DATA,$b,1);print(chr((ord($a) & $c) ^ ord($b)))}' -- "$@"
+exec perl -MScalar::Util=looks_like_number -e'
+sub bitops ($$$) {
+    my ($bits, $x, $y) = @_;
+    my $len = length($x);
+    die q{Error, operators must be of the same length.} unless $len == length($y);
+    my $clear = $bits ^ 0xff;
+    my $clearpack = unpack(q{Q}, chr($clear) x 8);
+    my $ofs = 0;
+    my $res = q{};
+    my (@a, @b, $a, $b);
+    @a = unpack(q{Q*}, $x);
+    @b = unpack(q{Q*}, $y);
+    my $i = 0;
+    for $a (@a) {
+        $res .= pack(q{Q}, ($clearpack & $a) ^ $b[$i++]);
+    }
+    $ofs = $len & ~7;
+    while ($ofs < $len) {
+        $a = ord(substr($x, $ofs, 1));
+        $b = ord(substr($y, $ofs, 1));
+        $res .= chr(($clear & $a) ^ $b);
+        $ofs++;
+    }
+    $res;
+}
+my $clear;
+if (@ARGV >= 1) {
+    die qq{Error, $ARGV[0] is not a number.} unless looks_like_number($ARGV[0]);
+    $clear = eval($ARGV[0]);
+}
+my $argscheck = join(@ARGV, @ARGV[1..$#ARGV]);
+if (@ARGV == 1 or $argscheck =~ m{^( - | - 3 - )$}x) {
+    while (read(STDIN, my $a, 4096)) {
+        print(bitops($clear, $a, $a));
+    }
+} elsif (@ARGV == 2) {
+    open(DATA, q{<}, $ARGV[1]);
+    while (read(STDIN, my $a, 4096)) {
+        read(DATA, my $b, length($a)) or die;
+        print(bitops($clear, $a, $b));
+    }
+    close(DATA);
+} elsif (@ARGV == 3) {
+    my $open_A = $ARGV[1] ne q{-};
+    my $open_B = $ARGV[2] ne q{-};
+    open(A, q{<}, $ARGV[1]) or die qq{$!: "$ARGV[1]"} if $open_A;
+    open(B, q{<}, $ARGV[2]) or die qq{$!: "$ARGV[2]"} if $open_B;
+    *A = *STDIN unless $open_A;
+    *B = *STDIN unless $open_B;
+    while (read(A, my $a, 4096)) {
+        read(B, my $b, length($a)) or die;
+        print(bitops($clear, $a, $b));
+    }
+    close(A) if $open_A;
+    close(B) if $open_B;
+} else {
+    die q{Error, invalid usage.};
+}' -- "$@"
