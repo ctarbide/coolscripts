ba1bcb347b5fb0e15ea68bc5a2a471653dd14b98
C. Tarbide <ctarbide@tuta.io>
2023-10-08 17:15:41 +0000

    simplify csprng.sh arguments processing

--- bin/csprng.sh
+++ bin/csprng.sh
@@ -9,7 +9,6 @@ if [ -t 1 ]; then
 fi
 
 key=
-args=
 
 while [ $# -gt 0 ]; do
     case "${1}" in
@@ -17,28 +16,23 @@ while [ $# -gt 0 ]; do
         --key=*) key=${1#*=} ;;
         -k*) key=${1#??} ;;
 
-        -) args="${args:+${args} }'-'" ;;
-        --) ;;
-        -*)
+        *)
             echo "${0##*/}: Unrecognized option '${1}'." 1>&2
             exit 1
             ;;
-        *) args="${args:+${args} }'${1}'" ;;
     esac
     shift
 done
 
-set --
-
-if [ x"${key}" != x ]; then
-    args="${args:+${args} }'${key}'"
+if [ x"${key}" = x ]; then
+    key=-
 fi
 
-eval "set -- "'"$@"'" -- ${args}"
+set -- "${key}"
 
 exec perl -MDigest::SHA=sha512 -0777 -e'
     $_ = <>;
     $k = length($_).qq{:${_},};
     while (++$n) {
         print(sha512(qq{${k}${n}}));
-    }' "$@"
+    }' -- "$@"
