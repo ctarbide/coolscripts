15a6d75fe3c163c91df5db6cf5c393778efbd9f2
C. Tarbide <ctarbide@tuta.io>
2023-10-08 16:48:34 +0000

    allow bits-to-bytes and bytes-to-bits to use an explicit "-" to denote stdin

--- bin/bits-to-bytes.c
+++ bin/bits-to-bytes.c
@@ -19,14 +19,15 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#line 51 "bits-to-bytes-in-c.nw"
+#include <string.h>
+#line 52 "bits-to-bytes-in-c.nw"
 #define OK			0	/* status code for successful run */
 #define usage_error		1	/* status code for improper syntax */
 #define cannot_open_file	2	/* status code for file access error */
 #define READ_ONLY		0	/* read access code for system open */
 #define buf_size	BUFSIZ		/* stdio.h BUFSIZ chosen for efficiency */
 #define print_count(i,n) printf("%s%ld", i ? " " : "", n)
-#line 60 "bits-to-bytes-in-c.nw"
+#line 61 "bits-to-bytes-in-c.nw"
 int status = OK;	/* exit status of command, initially OK */
 char *prog_name;	/* who we are */
 /* total number of words, lines, chars */
@@ -34,7 +35,7 @@ long tot_word_count, tot_line_count, tot_char_count;
 #line 7 "bits-to-bytes-in-c.nw"
 int main(int argc, char **argv)
 {
-#line 132 "bits-to-bytes-in-c.nw"
+#line 139 "bits-to-bytes-in-c.nw"
 	int file_count;		/* how many files there are */
 	int fd = 0;		/* file descriptor, initialized to stdin */
 	char buffer[buf_size];	/* we read the input into this array */
@@ -42,35 +43,39 @@ int main(int argc, char **argv)
 	char *buf_end;		/* the first unused position in buffer */
 	int out;		/* output char */
 	ssize_t nc;		/* # of chars just read */
-#line 142 "bits-to-bytes-in-c.nw"
+#line 149 "bits-to-bytes-in-c.nw"
 	prog_name = argv[0];
-#line 128 "bits-to-bytes-in-c.nw"
+#line 135 "bits-to-bytes-in-c.nw"
 	file_count = argc - 1;
-#line 117 "bits-to-bytes-in-c.nw"
+#line 124 "bits-to-bytes-in-c.nw"
 	argc--;
 	do {
-#line 105 "bits-to-bytes-in-c.nw"
-		if (file_count > 0
-		    && (fd = open(*(++argv), READ_ONLY)) < 0) {
-			fprintf(stderr,
-				"%s: cannot open file %s\n",
-				prog_name, *argv);
-			status |= cannot_open_file;
-			file_count--;
-			continue;
+#line 108 "bits-to-bytes-in-c.nw"
+		if (file_count > 0) {
+			char *fname = *(++argv);
+			if (strcmp(fname, "-") == 0) {
+				fd = 0;
+			} else if ((fd = open(fname, READ_ONLY)) < 0) {
+				fprintf(stderr,
+					"%s: cannot open file %s\n",
+					prog_name, *argv);
+				status |= cannot_open_file;
+				file_count--;
+				continue;
+			}
 		}
-#line 101 "bits-to-bytes-in-c.nw"
+#line 104 "bits-to-bytes-in-c.nw"
 		ptr = buf_end = buffer;
-#line 85 "bits-to-bytes-in-c.nw"
+#line 88 "bits-to-bytes-in-c.nw"
 		while (1) {
-#line 76 "bits-to-bytes-in-c.nw"
+#line 79 "bits-to-bytes-in-c.nw"
 			if (ptr + 16 > buf_end) {
 				ptr = buffer;
 				nc = read(fd, ptr, buf_size);
 				if (nc <= 0) break;
 				buf_end = buffer + nc;
 			}
-#line 87 "bits-to-bytes-in-c.nw"
+#line 90 "bits-to-bytes-in-c.nw"
 			out =	((ptr[0*2] & 1) << 7) |
 				((ptr[1*2] & 1) << 6) |
 				((ptr[2*2] & 1) << 5) |
@@ -82,12 +87,14 @@ int main(int argc, char **argv)
 			ptr += 16;
 			putc(out, stdout);
 		}
-#line 70 "bits-to-bytes-in-c.nw"
-		close(fd);
-#line 123 "bits-to-bytes-in-c.nw"
+#line 71 "bits-to-bytes-in-c.nw"
+		if (fd != 0) {
+			close(fd);
+		}
+#line 130 "bits-to-bytes-in-c.nw"
 		/* even if there is only one file */
 	} while (--argc > 0);
-#line 146 "bits-to-bytes-in-c.nw"
+#line 153 "bits-to-bytes-in-c.nw"
 	exit(status);
 	return 0;
 #line 14 "bits-to-bytes-in-c.nw"
--- bin/bytes-to-bits.c
+++ bin/bytes-to-bits.c
@@ -19,14 +19,15 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#line 51 "bytes-to-bits-in-c.nw"
+#include <string.h>
+#line 52 "bytes-to-bits-in-c.nw"
 #define OK			0	/* status code for successful run */
 #define usage_error		1	/* status code for improper syntax */
 #define cannot_open_file	2	/* status code for file access error */
 #define READ_ONLY		0	/* read access code for system open */
 #define buf_size	BUFSIZ		/* stdio.h BUFSIZ chosen for efficiency */
 #define print_count(i,n) printf("%s%ld", i ? " " : "", n)
-#line 60 "bytes-to-bits-in-c.nw"
+#line 61 "bytes-to-bits-in-c.nw"
 int status = OK;	/* exit status of command, initially OK */
 char *prog_name;	/* who we are */
 /* total number of words, lines, chars */
@@ -34,7 +35,7 @@ long tot_word_count, tot_line_count, tot_char_count;
 #line 7 "bytes-to-bits-in-c.nw"
 int main(int argc, char **argv)
 {
-#line 134 "bytes-to-bits-in-c.nw"
+#line 141 "bytes-to-bits-in-c.nw"
 	int file_count;		/* how many files there are */
 	int fd = 0;		/* file descriptor, initialized to stdin */
 	char buffer[buf_size];	/* we read the input into this array */
@@ -42,41 +43,45 @@ int main(int argc, char **argv)
 	char *buf_end;		/* the first unused position in buffer */
 	int c;			/* current char */
 	char out[8*2+1] = {	/* all bits of a byte with \n and \0 */
-#line 130 "bytes-to-bits-in-c.nw"
+#line 137 "bytes-to-bits-in-c.nw"
 		0, '\n', 0, '\n', 0, '\n', 0, '\n',
-#line 130 "bytes-to-bits-in-c.nw"
+#line 137 "bytes-to-bits-in-c.nw"
 		0, '\n', 0, '\n', 0, '\n', 0, '\n',
 		0};
 	ssize_t nc;		/* # of chars just read */
-#line 148 "bytes-to-bits-in-c.nw"
+#line 155 "bytes-to-bits-in-c.nw"
 	prog_name = argv[0];
-#line 126 "bytes-to-bits-in-c.nw"
+#line 133 "bytes-to-bits-in-c.nw"
 	file_count = argc - 1;
-#line 115 "bytes-to-bits-in-c.nw"
+#line 122 "bytes-to-bits-in-c.nw"
 	argc--;
 	do {
-#line 103 "bytes-to-bits-in-c.nw"
-		if (file_count > 0
-		    && (fd = open(*(++argv), READ_ONLY)) < 0) {
-			fprintf(stderr,
-				"%s: cannot open file %s\n",
-				prog_name, *argv);
-			status |= cannot_open_file;
-			file_count--;
-			continue;
+#line 106 "bytes-to-bits-in-c.nw"
+		if (file_count > 0) {
+			char *fname = *(++argv);
+			if (strcmp(fname, "-") == 0) {
+				fd = 0;
+			} else if ((fd = open(fname, READ_ONLY)) < 0) {
+				fprintf(stderr,
+					"%s: cannot open file %s\n",
+					prog_name, *argv);
+				status |= cannot_open_file;
+				file_count--;
+				continue;
+			}
 		}
-#line 99 "bytes-to-bits-in-c.nw"
+#line 102 "bytes-to-bits-in-c.nw"
 		ptr = buf_end = buffer;
-#line 83 "bytes-to-bits-in-c.nw"
+#line 86 "bytes-to-bits-in-c.nw"
 		while (1) {
-#line 74 "bytes-to-bits-in-c.nw"
+#line 77 "bytes-to-bits-in-c.nw"
 			if (ptr >= buf_end) {
 				ptr = buffer;
 				nc = read(fd, ptr, buf_size);
 				if (nc <= 0) break;
 				buf_end = buffer + nc;
 			}
-#line 85 "bytes-to-bits-in-c.nw"
+#line 88 "bytes-to-bits-in-c.nw"
 			c = *ptr++;
 			out[0*2] = '0' | ((c & 0x80) >> 7);
 			out[1*2] = '0' | ((c & 0x40) >> 6);
@@ -88,12 +93,14 @@ int main(int argc, char **argv)
 			out[7*2] = '0' | ((c & 1) >> 0);
 			fputs(out, stdout);
 		}
-#line 70 "bytes-to-bits-in-c.nw"
-		close(fd);
-#line 121 "bytes-to-bits-in-c.nw"
+#line 71 "bytes-to-bits-in-c.nw"
+		if (fd != 0) {
+			close(fd);
+		}
+#line 128 "bytes-to-bits-in-c.nw"
 		/* even if there is only one file */
 	} while (--argc > 0);
-#line 152 "bytes-to-bits-in-c.nw"
+#line 159 "bytes-to-bits-in-c.nw"
 	exit(status);
 	return 0;
 #line 14 "bytes-to-bits-in-c.nw"
