7f45f6adf04b95f1d91c4a15c19305124cfac393
C. Tarbide <ctarbide@tuta.io>
2023-12-25 14:41:53 +0000

    fix removal of temporary files in nofake-exec.sh

--- bin/lynx-list-links.sh
+++ bin/lynx-list-links.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-user_agent=${USER_AGENT:-'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'}
+user_agent=${USER_AGENT:-'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'}
 for i in "$@"; do
     echo
     echo "input args: ${i}"
