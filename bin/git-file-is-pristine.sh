55cfeb38814bdc5e44bc9e0d20c239759f9de5de
C. Tarbide <ctarbide@tuta.io>
2024-01-10 17:26:14 +0000

    let git-file-is-pristine.sh accept multiple files

--- bin/git-file-is-pristine.sh
+++ bin/git-file-is-pristine.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # usage: git-file-is-pristine.sh git-file-is-pristine.sh && echo is pristine
 set -eu
-file=$1; shift
+for file do
+    # does it even exists?
+    [ -e "${file}" ]
 
-# does it even exists?
-[ -e "${file}" ]
+    # not versioned implies nonpristine
+    other=`git ls-files -o -- "${file}"`
+    [ x"${other}" = x ]
 
-# not versioned implies nonpristine
-other=`git ls-files -o -- "${file}"`
-[ x"${other}" = x ]
-
-# versioned and without differences to working copy?
-exec git diff-index --quiet HEAD -- "${file}"
+    # versioned and without differences to working copy?
+    git diff-index --quiet HEAD -- "${file}"
+done
