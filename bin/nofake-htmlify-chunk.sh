ef6ee84df807343c6f45b3474525aa4adae84329
C. Tarbide <ctarbide@tuta.io>
2024-02-05 15:09:50 +0000

    improve nofake-htmlify-chunk.sh

--- bin/nofake-htmlify-chunk.sh
+++ bin/nofake-htmlify-chunk.sh
@@ -4,16 +4,18 @@ set -eu
 die(){ ev=$1; shift; for msg in "$@"; do echo "${msg}"; done; exit "${ev}"; }
 [ "${#}" -ge 1 ] || die 1 "usage: nofake-htmlify-chunk.sh 'chunk name' file.nw"
 chunk=$1; shift
-nofake-split.pl "$@" | perl -le'$chunk = $ARGV[0]; while(<STDIN>){ chomp;
-    if (m{^${id}_2: (.*)}) {
-        $_ = $1;
-        s,&,&amp;,g;
-        s,<,&lt;,g;
-        s,>,&gt;,g;
-        print;
-        next;
+escape(){
+    perl -lpe's,&,&amp;,g; s,<,&lt;,g; s,:,&colon;,g; s,^\@,&#x40;,' -- "$@"
+}
+nofake-split.pl "$@" | perl -le'$chunk = $ARGV[0]; while(<STDIN>){
+    chomp;
+    if ($id) {
+        if (m{^${id}_2: (.*)}) {
+            print $1;
+            next;
+        }
+        undef $id;
     }
-    undef $id;
     next unless m{^([0-9a-f]*)_1: <<(.*)>>=\s*$};
-    $id=$1 if $2 eq $chunk
-}' -- "${chunk}"
+    $id = $1 if $2 eq $chunk;
+}' -- "${chunk}" | escape
