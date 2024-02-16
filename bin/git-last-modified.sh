00bda830d6add72de312f861defca6a8a80d1720
C. Tarbide <ctarbide@tuta.io>
2024-02-16 17:46:35 +0000

    improve bin/git-last-modified.sh to use "git rev-parse"

--- bin/git-last-modified.sh
+++ bin/git-last-modified.sh
@@ -14,22 +14,21 @@ set -eu
 # will output the date of the last commit relevant to the file
 #
 die(){ ev=$1; shift; for msg in "$@"; do echo "${msg}"; done; exit "${ev}"; }
-
-git_prefix(){ git -c core.quotepath=off rev-parse --show-prefix; }
-git_lstree(){ git -c core.quotepath=off ls-tree --full-tree -r -t -l HEAD; }
-
-get_blob_hash(){
-    path_expr='^[0-7]+ \s+ blob \s+ [0-9a-f]+ \s+ \d+ \s+ \Q'"${prefix}"'\E '"${filename}"' $'
-    git_lstree | perl -lane'next unless m{'"${path_expr}"'}x; print $F[2]; last'
+git_find_blob(){
+    treeish=$1; shift
+    filename=$1; shift
+    if [ x"${filename#.*/}" = x"${filename}" ]; then
+        filename=./${filename}
+    fi
+    git rev-parse "${treeish}:${filename}"
 }
 
-FORMAT=${FORMAT:-format:%B %e, %Y at %T UTC}
+FORMAT=${FORMAT:-format:%s %B %e, %Y at %T UTC}
 
-prefix=`git_prefix`
-filename=${1}
-
-blob=`get_blob_hash "${1}"`
-
-perl -le'exit($ARGV[0] !~ m{^[0-9a-f]+$}i)' -- "${blob}" || die 1 "Error, file \"${filename}\" not found in git history." 1>&2
-
-git log -1 --format=%cd --date="${FORMAT}" --find-object="${blob}" HEAD
+for filename; do
+    blob=`git_find_blob HEAD "${filename}"`
+    if [ x"${blob}" = x ]; then
+        die 1 "Error, could not find the blob for file \"${filename}\"" 1>&2
+    fi
+    git log -1 --format=%cd --date="${FORMAT}" --find-object="${blob}" HEAD
+done
