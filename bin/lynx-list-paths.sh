9725c4582a9418053c20bb47d55a39fea51146cb
C. Tarbide <ctarbide@tuta.io>
2024-02-19 01:32:51 +0000

    add lynx-list-paths.sh

---
+++ bin/lynx-list-paths.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# https://ctarbide.github.io/pages/2024/2024-02-05_12h00m11_hello-worlds/
+# https://github.com/ctarbide/coolscripts/blob/master/bin/nofake-exec.nw
+set -eu; set -- "${0}" --ba-- "${0}" "$@" --ea--
+set -- "$@" --tmp-- .input
+SH=${SH:-sh}; export SH
+PERL=${PERL:-perl}; export PERL
+exec nofake-exec.sh --error -Rprog "$@" -- "${SH}" -eu
+exit 1
+
+<<prog>>=
+thisprog=${1}; shift # the initial script
+<<slurp to .input>>
+set -- "${thisprog}" --aa-- "${0}.input"
+nofake-exec.sh --error -Rlynx-list-paths "$@" -- "${PERL}" | LC_ALL=C sort -u
+@
+
+Must slurp and save input because 'lynx-list-paths' do two passes on the file.
+
+<<slurp to .input>>=
+cat -- "$@" >"${0}.input"
+@
+
+<<lynx-list-paths>>=
+use 5.008; # perl v5.8.0 was released on July 18, 2002
+use strict;
+use warnings FATAL => qw{uninitialized void inplace};
+use Carp ();
+
+local $\ = "\n";
+
+my $carp_or_croak = \&Carp::carp;
+
+my %dirs;
+
+my @args = @ARGV;
+
+Carp::croak("Error, no input file") unless @args;
+
+# STDIN is a pipe and cannot be rewinded/reopened
+Carp::croak("Error, input file cannot be standard input") if grep { $_ eq '-' } @args;
+
+@ARGV = @args;
+while (<>) {
+    chomp;
+    next unless m{^ \s* \d+ \. \s+ ([hf][tps]+ :// .*) $}xi;
+    $_ = $1;
+    s,^(.*/)(.*?)$,${1},; # dirname
+    $dirs{${1}}++;
+}
+
+print "'${_}'" for keys %dirs;
+
+@ARGV = @args;
+while (<>) {
+    chomp;
+    s,#.+$,,;
+    next unless m{^ \s* \d+ \. \s+ ([hf][tps]+ :// .* [^/]) $}xi;
+    unless ($dirs{"${1}/"}) {
+        print "'${1}'";
+    }
+}
+@
