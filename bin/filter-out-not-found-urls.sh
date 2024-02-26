4625ac2ed344ed937af8ff6bca274ff6e97a2522
C. Tarbide <ctarbide@tuta.io>
2024-02-26 00:10:52 +0000

    minor improvements

--- bin/filter-out-not-found-urls.sh
+++ bin/filter-out-not-found-urls.sh
@@ -3,11 +3,11 @@ set -eu
 LC_ALL=C
 export LC_ALL
 filter(){
-    perl -lne's,^\047(.*)\047$,${1},; s,\047,%27,g; next if m{\?$}; print' -- "$@"
+    perl -lne's,^\047(.*)\047$,${1},; s,\047,%27,g; next if m{\?$}; print(qq{\047${_}\047})' -- "$@"
 }
 if [ -f not-found ]; then
     if [ ! -f not-found.sorted -o not-found -nt not-found.sorted ]; then
-        perl -lne's,^--2[\d\-: ]+--\s+,,; next unless m{^[\042\047]?(\w+://.*?)[\042\047]?$}; print($1)' -- not-found |
+        perl -lne's,^--2[\d\-: ]+--\s+,,; next unless m{^[\042\047]?(\w+://.*?)[\042\047]?$}; print(qq{\047${1}\047})' -- not-found |
             sort -u > not-found.sorted
     fi
     filter "$@" | comm -23 - not-found.sorted
