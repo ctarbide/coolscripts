a7fa3298c907c4c60df2063a26cfdace3c41e532
C. Tarbide <ctarbide@tuta.io>
2024-03-01 01:13:22 +0000

    update nofake from ctweb and add normalization of arguments to nofake-exec.sh

--- bin/nofake-exec.sh
+++ bin/nofake-exec.sh
@@ -53,6 +53,21 @@ temporary_file(){
     fi
     echo "${tmpfile}"
 }
+normalize_arg(){
+    head=
+    tail=${1}
+    acc=
+    while true; do
+        head=${tail%%\'*}
+        tail=${tail#*\'}
+        acc=${acc:+${acc}"'\"'\"'"}${head}
+        if [ x"${tail}" = x"${head}" ]; then
+            # didn't advance
+            break;
+        fi
+    done
+    echo "${acc}"
+}
 
 SRC_PREFIX=${SRC_PREFIX:-}
 NOFAKE_SH=${NOFAKE_SH:-nofake.sh}
@@ -65,8 +80,11 @@ nargs= # nofake args
 eargs= # exec args
 
 for arg do
-    if [ x"${arg}" = x-- ]; then shift; break; fi
-    nargs="${nargs:+${nargs} }'${arg}'"
+    if [ x"${arg}" = x-- ]; then
+        shift
+        break
+    fi
+    nargs="${nargs:+${nargs} }'`normalize_arg "${arg}"`'"
     shift # 'for' is synchronized with 'shift'
 done
 
@@ -74,7 +92,9 @@ if [ x$# = x0 ]; then
     die 1 "Error, wrong usage, exec arguments are absent." 1>&2
 fi
 
-for arg do eargs="${eargs:+${eargs} }'${arg}'"; done
+for arg do
+    eargs="${eargs:+${eargs} }'`normalize_arg "${arg}"`'"
+done
 
 eval "set -- ${nargs}"
 
@@ -87,16 +107,16 @@ tmps=
 
 while [ $# -gt 0 ]; do
     case "${1}" in
-        -L*|--error) opts="${opts:+${opts} }'${1}'" ;;
-        -R*) chunks="${chunks:+${chunks} }'${1}'" ;;
+        -L*|--error) opts="${opts:+${opts} }'`normalize_arg "${1}"`'" ;;
+        -R*) chunks="${chunks:+${chunks} }'`normalize_arg "${1}"`'" ;;
 
-        -o|--output) output=${2}; shift ;;
-        --output=*) output=${1#*=} ;;
-        -o*) output=${1#??} ;;
+        -o|--output) output=`normalize_arg "${2}"`; shift ;;
+        --output=*) output=`normalize_arg "${1#*=}"` ;;
+        -o*) output=`normalize_arg "${1#??}"` ;;
 
-        --tmp--) tmps="${tmps:+${tmps} }'${2}'"; shift ;;
+        --tmp--) tmps="${tmps:+${tmps} }'`normalize_arg "${2}"`'"; shift ;;
 
-        --aa--) appendargs="${appendargs:+${appendargs} }'${2}'"; shift ;;
+        --aa--) appendargs="${appendargs:+${appendargs} }'`normalize_arg "${2}"`'"; shift ;;
         --dd--) appendargs="${appendargs:+${appendargs} }'--'" ;;
 
         --ba--) # begin args
@@ -106,17 +126,18 @@ while [ $# -gt 0 ]; do
                     # end args
                     break
                 fi
-                appendargs="${appendargs:+${appendargs} }'${1}'"
+                appendargs="${appendargs:+${appendargs} }'`normalize_arg "${1}"`'"
                 shift # 'for' is synchronized with 'shift'
             done
-	    ;;
+            ;;
 
         -) sources="${sources:+${sources} }'-'" ;;
         -*)
             ${ECHO_ERROR} "${0##*/}: Unrecognized option '${1}'." 1>&2
             exit 1
             ;;
-        *) sources="${sources:+${sources} }'${SRC_PREFIX}${1}'" ;;
+
+        *) sources="${sources:+${sources} }'${SRC_PREFIX}`normalize_arg "${1}"`'" ;;
     esac
     shift
 done
---
+++ bin/normalize-args.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# tests:
+#
+#   ./normalize-args.sh <normalize-args.test
+#
+#   ./normalize-args.sh <normalize-args.test2
+#
+#   perl -lpe'$_ = qq{ ${_} }' <normalize-args.test | ./normalize-args.sh
+#
+#   perl -l040 -ne'print}{$\=qq{\n};print' <normalize-args.test | ./normalize-args.sh
+#
+#   ./normalize-args.sh <normalize-args.test | xargs ~/showargs-nl
+#
+#   ./normalize-args.sh <normalize-args.test2 | xargs ~/showargs-nl
+#
+#   cat normalize-args.test2 | xargs ~/showargs-nl
+#
+exec perl -MURI::Escape -lne'
+#my $line = $_;
+s,\\\s*$,,;
+s,%,%25,g;
+s,\134\134,%5c%5c,g;
+s,\134\047,%27,g;
+s,\134\042,%22,g;
+s,\134,%5c,g;
+s{
+    (?:([^\047\042\s]*)\047) ( .*? ) (?:\047([^\047\042\s]*)) |
+    (?:([^\047\042\s]*)\042) ( .*? ) (?:\042([^\047\042\s]*)) |
+    ( [^\047\042\s]+ )
+}{
+    #print("[1:$1][2:$2][3:$3][4:$4][5:$5][6:$6][7:$7][${line}]");
+    if (defined($2)) {
+        $s = qq{${1}${2}${3}};
+    } elsif (defined($5)) {
+        my $s4 = $4;
+        my $s6 = $6;
+        ($s = $5) =~ s,%27|\047,\047\042\047\042\047,g;
+        $s = qq{${s4}${s}${s6}};
+    } elsif ($7) {
+        next if $7 eq q{%5c}; # only a line continuation
+        ($s = $7) =~ s,%5c$,,; # remove line continuation, if any
+    } else {
+        $s = q{exhaustion};
+    }
+    if ($s) {
+        $s = qq{\047} . uri_unescape($s) . qq{\047};
+        $s =~ s,^\047\047|\047\047$,,g;
+        print $s;
+    } else {
+        print qq{\047\047};
+    }
+}gex' -- "$@"
