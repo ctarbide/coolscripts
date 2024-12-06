979034f030cdd7a7480ea5768d70b0a04b2b4624
C. Tarbide <ctarbide@tuta.io>
2024-12-06 14:09:21 +0000

    let feh-smart-latest.sh use --filelist instead of command line arguments

--- bin/feh-smart-latest.sh
+++ bin/feh-smart-latest.sh
@@ -7,13 +7,61 @@ die(){ ev=$1; shift; for msg in "$@"; do echo "${msg}"; done; exit "${ev}"; }
 LC_ALL=C
 export LC_ALL
 
-glob_has_file(){
-    perl -le'exit(0) for grep {-f} glob($ARGV[0]); exit(1)' "${1}"
+tmpfiles=
+
+rm_tmpfiles(){
+    eval "set -- ${tmpfiles}"
+    rm -f -- "$@"
 }
+
+# 0:exit, 1:hup, 2:int, 3:quit, 15:term
+trap 'rm_tmpfiles' 0 1 2 3 15
+
+u0Aa(){
+perl -le'
+@map = map {chr} 48..57, 65..90, 97..122;
+while (read(STDIN,$d,64)) {
+    for $x (unpack(q{C*},$d)) {
+        next if $x >= @map;
+        print $map[$x];
+    }
+}' </dev/urandom |
+    head -n"${1}" | perl -pe chomp; }
+
+r0Aa(){
+perl -le'
+@map = map {chr} 48..57, 65..90, 97..122;
+sub r{int(rand(scalar(@map)))}
+for (;;) { print $map[r] }' |
+    head -n"${1}" | perl -pe chomp; }
+
+create_safe_file(){
+    ( umask 0177; : >"${1}" )
+}
+
+# mktemp isn't portable, can't use it
+temporary_file(){
+    if command -v perl >/dev/null 2>&1 && test -r /dev/urandom; then
+        tmpfile="/tmp/tmp.`u0Aa 10`${1:-}"
+        create_safe_file "${tmpfile}"
+    elif command -v perl >/dev/null 2>&1; then
+        tmpfile="/tmp/tmp.`r0Aa 12`${1:-}"
+        create_safe_file "${tmpfile}"
+    else
+        die 1 'error: mktemp not found'
+    fi
+    echo "${tmpfile}"
+}
+glob_has_file(){
+    perl -le'exit 0 for grep {-f} glob($ARGV[0]); exit 1' "${1}"
+}
+
+filelist=`temporary_file`
+tmpfiles="${tmpfiles:+${tmpfiles} }'${filelist}'"
 
 if [ "$#" -le 0 ]; then
     if glob_has_file "${HOME}/Downloads/Pictures/Screenshot_*.png"; then
-        set -- ~/Downloads/Pictures/Screenshot_*.png
+        ls -1 ~/Downloads/Pictures/Screenshot_*.png >>"${filelist}"
     fi
 else
     for i in "$@"; do
@@ -22,16 +70,16 @@ else
         fi
     done
     years=$@
-    set -- # clear $@
     for i in $years; do
         if glob_has_file "${HOME}/Downloads/Pictures/${i}/Screenshot_*.png"; then
-            set -- "$@" ~/Downloads/Pictures/"${i}"/Screenshot_*.png
+            ls -1 ~/Downloads/Pictures/"${i}"/Screenshot_*.png >>"${filelist}"
         fi
     done
 fi
 
-if [ "$#" -le 0 ]; then
+nimg=`wc -l "${filelist}"`
+if [ "${nimg}" -le 0 ]; then
     die 1 "error: no files to show"
 fi
 
-exec feh-smart.sh "$@"
+exec feh-smart.sh --filelist "${filelist}" "$@" --
