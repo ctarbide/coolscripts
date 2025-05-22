1407f8061fddb12ea0dce67970a5873a03ff98e1
C. Tarbide <ctarbide@tuta.io>
2025-05-22 13:38:19 +0000

    minor improvement

--- bin/curl-head-info.sh
+++ bin/curl-head-info.sh
@@ -5,3 +5,6 @@ echo '**** input args: '"$@"
 echo
 curl --head -sSL "$@" |
     perl -lne'next unless m{^(?: http | date | server | location | last-modified | content-type \s* : | content-length \s* : | \s )}xi; print'
+if [ x"${WAIT:-}" != x ]; then
+    perl -sle'select(undef,undef,undef,$wait);' -- -wait="${WAIT}"
+fi
